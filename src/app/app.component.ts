2162780742cd63dd49e0420228995ebca986ff44
retr0byte <dev.gbcproject@gmail.com>
2022-03-25 21:23:31 -0300

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,40 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  tasks: string[];
+  curTask: string;
+  warning: boolean;
+
+  constructor() {
+    this.tasks = [];
+    this.curTask = '';
+    this.warning = false;
+  }
+
+  add(): void | null {
+    if (this.curTask === '') {
+      this.warning = true;
+      return null;
+    }
+    this.tasks.push(this.curTask);
+    this.warning = false;
+    this.curTask = '';
+    this.save();
+  }
+
+  remove(taksId: number): void {
+    this.tasks.splice(taksId, 1);
+    this.save();
+  }
+
+  show(): string[] {
+    return this.tasks;
+  }
+
+  private save(): void {
+    localStorage.setItem('userTaks', JSON.stringify(this.tasks));
+  }
 }
